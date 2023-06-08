c15534f9dc126c59de2e60af58dd1d2564aff08a
Musong <ernestakeh@gmail.com>
2023-06-08 05:52:38 +0100

    Add methods to process data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
