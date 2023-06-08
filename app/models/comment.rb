a4846dc024d6608370ee5adabff0bf8ff93bea17
Musong <ernestakeh@gmail.com>
2023-06-08 11:56:32 +0100

    Remove recent_comments method from private method in Post model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   after_save :update_post_comments_counter
 
-  private
-
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,13 @@ class Post < ApplicationRecord
   has_many :likes
   after_save :update_user_posts_counter
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
 
   def update_user_posts_counter
     author.increment!(:posts_counter)
-  end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
   end
 end
