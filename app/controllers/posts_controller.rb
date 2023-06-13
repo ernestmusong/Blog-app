56ab33196fa0ab8ad5336a2d91b186fd358c00ed
Musong <ernestakeh@gmail.com>
2023-06-13 04:08:28 +0100

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index; end
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root '/users/', to: 'users#index', as: 'users'
+  root 'users#index'
+  get '/users/', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
