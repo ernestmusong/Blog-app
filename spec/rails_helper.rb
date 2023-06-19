69665122f8473db5d1032bb9e76ac4e6e27ff2f8
Musong <ernestakeh@gmail.com>
2023-06-20 00:49:17 +0100

    Add integration tests for users index action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'capybara/rspec'
+require 'webdrivers'
+
+
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'faker'
@@ -65,3 +70,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
