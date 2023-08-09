aacdd760b032619bc96b30de70f4752e89679507
Musong <ernestakeh@gmail.com>
2023-08-09 09:20:11 +0100

    Add redirect path after signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(_resource)
+    user_path(current_user)
+  end
+
+  def after_sign_in_path_for(_resource)
+    user_path(current_user)
+  end
+
   protected
 
   def update_allowed_parameters
